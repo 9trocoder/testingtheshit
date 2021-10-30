6ecca885ab57cf1e5e3168aa29a26d471ca56f2c
Emmanuel Oladimeji Adeyemi <46575938+yemicodes@users.noreply.github.com>
2021-10-31 02:13:08 +0300

    Initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// firebase config
+export const firebaseConfig = {
+  apiKey: "AIzaSyDexvJjg-oaUBexhNeDIpfmE5BMvt4T6x0",
+  authDomain: "leadroitt-db.firebaseapp.com",
+  databaseURL: "https://leadroitt-db-default-rtdb.firebaseio.com",
+  projectId: "leadroitt-db",
+  storageBucket: "leadroitt-db.appspot.com",
+  messagingSenderId: "68539723221",
+  appId: "1:68539723221:web:4d717b08171089e1cd215c",
+  measurementId: "G-B7SVHQKS95",
+};
